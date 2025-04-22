76b4e32ac15c9d5a11c53172991a354cc97c4dcf
Natalia <57619785+MebiusCat@users.noreply.github.com>
2025-04-22 16:34:26 +0300

    injections

--- SQLAlchemy_Playground/my_app/db_app/app.py
+++ SQLAlchemy_Playground/my_app/db_app/app.py
@@ -1,56 +1,30 @@
-import os
-import psycopg2
-
-from dotenv import load_dotenv
 from typing import List
-import uvicorn
 
 from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
 
-from .schema_db import SessionLocal, Booking, Facility, Member
-from .models import BookingGet, UserGet, FacilityGet
-
+from .database import SessionLocal
+from .models import Booking, Facility, Member
+from .schemas import BookingGet, UserGet
 
 app = FastAPI()
 
-
-def get_db_postgresql():
-    with psycopg2.connect(
-        user=os.environ['POSTGRES_USER'],
-        password=os.environ['POSTGRES_PASSWORD'],
-        host=os.environ['POSTGRES_HOST'],
-        port=os.environ['POSTGRES_PORT'],
-        database=os.environ['POSTGRES_DATABASE']
-    ) as conn:
-        return conn
 
 def get_db():
     with SessionLocal() as db:
         return db
 
 
-@app.get('/user/all', response_model=List[UserGet])
-def show_users(limit: int = 10, db: Session = Depends(get_db)):
+@app.get("/user/all", response_model=List[UserGet])
+def get_all_users(limit: int = 10, db: Session = Depends(get_db)):
     return db.query(Member).limit(limit).all()
 
 
-@app.get('/bookings/show', response_model=List[BookingGet])
-def show_bookings(limit: int = 10, db: Session = Depends(get_db)):
-    return db.query(Booking).limit(limit).all()
-
-
-@app.get('/facilities', response_model=List[FacilityGet])
-def show_bookings(limit: int = 10, db: Session = Depends(get_db)):
+@app.get("/facility/all")
+def get_all_facilities(limit: int = 10, db: Session = Depends(get_db)):
     return db.query(Facility).limit(limit).all()
 
 
-@app.get('/bookings/hacks')
-def show_bookings(limit: int = 10, db: Session = Depends(get_db)):
-    for el in db.query(Booking).limit(limit).all():
-        print(f'{el.member_rl.surname}')
-
-
-if __name__ == "__main__":
-    load_dotenv()
-    uvicorn.run(app)
+@app.get("/booking/all", response_model=List[BookingGet])
+def get_all_bookings(limit: int = 10, db: Session = Depends(get_db)):
+    return db.query(Booking).limit(limit).all()
